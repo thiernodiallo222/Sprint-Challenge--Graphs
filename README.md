45d55ee05246134ebde7e117fec86d3a4cfa832c
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-08-29 16:07:40 -0400

    half way there

--- README.md
+++ README.md
@@ -13,7 +13,6 @@ Open `adv.py`. There are four parts to the provided code:
 You may find the commands `player.current_room.id`, `player.current_room.get_exits()` and `player.travel(direction)` useful.
 
 To solve this path, you'll want to construct your own traversal graph. You start in room `0`, which contains exits `['n', 's', 'w', 'e']`. Your starting graph should look something like this:
-
 ```
 {
   0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}
--- adv.py
+++ adv.py
@@ -1,3 +1,4 @@
+
 from room import Room
 from player import Player
 from world import World
@@ -28,6 +29,69 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+# sets up directions in order to "reverse" If we moved east previously it makes it west
+reversedir = {"n": "s", "s": "n", "e": "w", "w": "e"}
+reverse = [None]
+
+# route for knowing where I've been.
+
+# before refactor
+# route = visited = {}  # Note that this is a dictionary, not a set
+# before refactor
+
+# after refactor
+# Didnt effect number of moves :/
+visited = {}  # Note that this is a dictionary, not a set
+route = []
+DIR = ""
+# after refactor
+
+# runs while we have not yet been to all rooms in world
+while len(visited) < len(room_graph):
+    local = player.current_room.id
+    print("Local Room Number is ===>", local)
+    # gets exits to the local room
+    localexits = player.current_room.get_exits()
+
+    # before refactor
+    # DIR = ""
+    # before refactor
+
+    # adds local room to route
+    # after refactor
+    route.append({local: DIR})
+    # after refactor
+
+    if local not in visited:
+        # if we haven't just started.
+        if reverse[-1]:
+            # remove the most recently used dir from exits so we don't go back into it when we return to prev room.
+            localexits.remove(reverse[-1])
+        # sets exits to local to visited
+        visited[local] = localexits
+
+# if we have directions to go / room has exits other than the one we came in
+#     will try to go east first, then south west north
+
+    if len(visited[local]) > 0:
+        # pop off the last one we were just in
+        DIR = visited[local].pop()
+        # put it in the map
+        traversal_path.append(DIR)
+        # map the reverse direction
+        reverse.append(reversedir[DIR])
+        # MOVETO if there is a "forward" path
+        print("The Journey continues! Move Forward", DIR)
+        player.travel(DIR)
+    # if we hit a dead end, initiates reverse and backtracks along path
+    else:
+        # take off the most recent direction and move the opposite direction until there are available exits
+        if reverse[-1]:
+            DIR = reverse.pop()
+            traversal_path.append(DIR)
+            print("Oof Move Back", DIR)
+            player.travel(DIR)
 
 
 
